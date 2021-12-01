cb2252d9ab0c9b968f1dd95f2c411dea6b3ee36e
thibault <thibault.blanchard@edu.itescia.fr>
2021-12-01 16:48:22 +0100

    Liste .txt en list python

---
+++ words_library.py
@@ -0,0 +1,13 @@
+
+def import_library():
+
+    #Get the liste_francais.txt and pass it througt a Python list named library
+
+    extract = open("liste_francais.txt")
+    library = []
+    library = extract.readlines()
+
+    print(library[76])
+
+def random_words_library():
+    pass
