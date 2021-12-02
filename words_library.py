8df0bab42d8fde8a5cad956dbc22936523c73295
thibault <thibault.blanchard@edu.itescia.fr>
2021-12-02 11:05:09 +0100

    cut words in letter

--- words_library.py
+++ words_library.py
@@ -12,31 +12,21 @@ def import_library():
 
     random_words_library(library)
 
-def random_words_library(list):
+def random_words_library(extracted_list):
 
     word_list = []
     i = 0
     while i < 4:#difficulté select
 
         i += 1
-        word = random.choice(list)
-        word_list.extend([word])
+        word = random.choice(extracted_list)
+        word_list.extend(word)
+
+    #Cut words in letters
+
+    word_list = list(map(lambda st: str.replace(st, "\n", ','), word_list))
 
     print(word_list)
-
-
-
-#     cut_word(word_list)
-#
-# def cut_word (list):
-#     i=0
-#     letter_list = []
-#     for letter in list:
-#         i+=1
-#         letter_list = list(list[i])
-#
-#
-#     print(letter_list)
 
 
 
