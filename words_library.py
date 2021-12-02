811353dcbc52fc2531725899b3a4fb14e32d934f
thibault <thibault.blanchard@edu.itescia.fr>
2021-12-02 10:20:37 +0100

    stock random word from list into another list

--- words_library.py
+++ words_library.py
@@ -3,22 +3,41 @@ import random
 
 def import_library():
 
-    #Get the liste_francais.txt and pass it througt a Python list named library
+    #Get liste_francais.txt and pass it througt a Python list named library
 
     extract = open("liste_francais.txt")
     library = extract.readlines()
 
     library = list(map(lambda st: str.replace(st, "?", "e"), library))
 
-    print(library)
-
     random_words_library(library)
 
 def random_words_library(list):
 
-    i=0
+    word_list = []
+    i = 0
     while i < 4:#difficulté select
-        i+=1
-        print(random.choice(list))
+
+        i += 1
+        word = random.choice(list)
+        word_list.extend([word])
+
+    print(word_list)
 
 
+
+#     cut_word(word_list)
+#
+# def cut_word (list):
+#     i=0
+#     letter_list = []
+#     for letter in list:
+#         i+=1
+#         letter_list = list(list[i])
+#
+#
+#     print(letter_list)
+
+
+
+#if len(world list) > nombre cases
